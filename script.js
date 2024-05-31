c81b6f9fd13f22fa39231ad464fd4714e04cc2af
Qajay0832 <123375564+Qajay0832@users.noreply.github.com>
2024-05-31 15:46:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+const button =document.getElementById("enterBtn");
+let oldElement=document.getElementById("status");
+button.onclick=(()=>oldElement.innerHTML="<h1>Entered Metaverse</h1>")
